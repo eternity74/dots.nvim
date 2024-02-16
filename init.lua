23d966678064e71121b87842a26597d8107e82bc
Wanchang Ryu <eternity74@gmail.com>
2024-02-16 15:07:05 +0900

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,4 +1,5 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+-- print(lazypath)
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
     "git",
@@ -24,25 +25,30 @@ local opts = {
 }
 
 require("lazy").setup("plugins", opts)
---local tele_plug = require("plugins.telescope")
---require("lazy").setup(tele_plug)
 require("options")
---[[
-require("conform").setup({
-	formatters_by_ft = {
-		lua = { "stylua" },
-		-- Conform will run multiple formatters sequentially
-		python = { "isort", "black" },
-		-- Use a sub-list to run only the first available formatter
-		javascript = { { "prettierd", "prettier" } },
-	},
-})
---]]
 
 vim.o.tabstop = 2
 vim.o.expandtab = true
 vim.o.softtabstop = 2
 vim.o.shiftwidth = 2
 
-local neogit = require("neogit")
-neogit.setup({})
+if vim.env.TMUX then
+  local i = vim.env.TMUX_UNIQUE:find(vim.env.TMUX_PANE, nil, true)
+  local session_name = require("b64").enc(string.sub(vim.env.TMUX_UNIQUE, 0, i - 1))
+  vim.o.shada = vim.o.shada .. ",n~/.local/state/nvim/shada/tmux-" .. session_name .. ".shada"
+  --print(vim.o.shada)
+end
+
+vim.api.nvim_create_autocmd({ "BufRead" }, {
+  pattern = { "COMMIT_EDITMSG" },
+  command = "set tw=72 colorcolumn=51,+1",
+})
+
+-- clang-format conf
+vim.g.clang_format_path = "/home/wanchang.ryu/bin/clang-format"
+vim.keymap.set(
+  { "n", "v" },
+  "<F5>",
+  ":py3f /home/wanchang.ryu/wtools/bin/clang-format.py<CR>",
+  { noremap = true }
+)
